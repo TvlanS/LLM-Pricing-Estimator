ddab22eb670f203129de44c1b8a4dc94689c3fcf
Tvlan <chikonut22@gmail.com>
2026-03-24 14:05:36 +0800

    update 1

--- utils/config_step.py
+++ utils/config_step.py
@@ -9,5 +9,4 @@ class Config():
             self.api_key = config["deepseek"]["api_key"]
             self.website_url = config["deepseek"]["website_url"]
             self.prompt = config["prompt"]["system_prompt2"]
-            self.xero_prompt = config["prompt"]["prompt_xero"]
             
